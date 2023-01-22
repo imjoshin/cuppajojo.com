ea9f15218e8ac851a36605f60f254f0619cb8f6c
Josh <jcjohnson77@gmail.com>
2023-01-22 15:39:01 -0500

    Fetch status every 30s

--- src/components/live-wrapper/LiveWrapper.tsx
+++ src/components/live-wrapper/LiveWrapper.tsx
@@ -1,15 +1,49 @@
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import * as styles from "./LiveWrapper.module.css"
 
 interface LiveWrapperProps {
   children: React.ReactNode,
 }
 
+type StatusType = { live: false } | {
+  live: true,
+  game: string,
+  title: string,
+  viewers: number,
+  started: string,
+  thumbnail: string,
+}
+
 export const LiveWrapper = ({ children }: LiveWrapperProps) => {
-  return (
-    <div>
-      {children}
-    </div>
-  )
+  const [status, setStatus] = useState<StatusType>({ live: false })
+
+  // set up interval and fetch status
+  useEffect(() => {
+    const fetchStatus = async () => {
+      const response = await window
+        .fetch(`/api/status`, {
+          method: `GET`,
+          headers: {
+            "content-type": "application/json",
+          },
+        })
+        .then(res => res.json())
+
+      setStatus(response)
+    }
+
+    fetchStatus()
+
+    // fetch status every 30 seconds
+    const interval = setInterval(fetchStatus, 30 * 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  return <>
+    {children}
+  </>
 }
