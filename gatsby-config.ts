5ce77eb91a25f95ea4c3612fd926f8e7fcb1a687
Josh <jcjohnson77@gmail.com>
2023-01-22 14:28:23 -0500

    Add manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,6 +22,24 @@ const config: GatsbyConfig = {
         maxVideos: 50 // Defaults to 50
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "CuppaJoJo",
+        short_name: "CuppaJoJo",
+        icon: 'src/images/jojo-head-small.png',
+        start_url: "/",
+        background_color: "#a471cc",
+        theme_color: "#a471cc",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -190,4 +190,8 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => {
+  return (
+    <title>CuppaJojo</title>
+  )
+}
