4163ea55264d37ce3c3d038549922985e595a8a0
Josh <jcjohnson77@gmail.com>
2023-01-23 00:43:22 -0500

    Actually use SEO tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { HeadFC, PageProps } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
 import { Home } from "../components/home"
 import { Layout } from "../components/layout"
+import { SEO } from "../components/seo"
 
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -18,19 +19,5 @@ const IndexPage: React.FC<PageProps> = () => {
 export default IndexPage
 
 export const Head: HeadFC = () => {
-  const { site } = useStaticQuery(
-    graphql`
-      query SiteMetaData {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
-
-  return (
-    <title>{site.siteMetadata.title}</title>
-  )
+  return <SEO />
 }
