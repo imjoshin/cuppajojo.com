cc571f5191ccc64b334d0a4d4cf536ce690a55f2
Josh <jcjohnson77@gmail.com>
2023-01-22 20:02:35 -0500

    Fetch game thumbnail too

--- src/api/status.js
+++ src/api/status.js
@@ -6,22 +6,37 @@ const THUMBNAIL_DIMENSIONS = {
   height: 1080 / 4,
 }
 
+const GAME_THUMBNAIL_DIMENSIONS = {
+  width: 285,
+  height: 380,
+}
+
 export default async function handler(req, res) {
   const twitch = new TwitchApi({
     client_id: process.env.TWITCH_CLIENT_ID,
     client_secret: process.env.TWITCH_CLIENT_SECRET
-  });
+  })
 
-  const streams = await twitch.getStreams({ channel: TWITCH_NAME });
+  const streams = await twitch.getStreams({ channel: TWITCH_NAME })
 
   let response = { live: false }
 
   if (streams && streams.data && streams.data.length) {
     const channel = streams.data[0]
 
+    let gameThumbnail = null
+    const games = await twitch.getGames(channel.game_id)
+
+    if (games && games.data && games.data.length) {
+      gameThumbnail = games.data[0].box_art_url
+        .replace('{width}', GAME_THUMBNAIL_DIMENSIONS.width)
+        .replace('{height}', GAME_THUMBNAIL_DIMENSIONS.height)
+    }
+
     response = {
       live: true,
       game: channel.game_name,
+      gameThumbnail,
       title: channel.title,
       viewers: channel.viewer_count,
       started: channel.started_at,
