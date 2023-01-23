7275d612b323fa358d6bd8f79c11d4fc43b5bab9
Josh <jcjohnson77@gmail.com>
2023-01-22 19:55:35 -0500

    Cleanup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,7 +22,7 @@ const config: GatsbyConfig = {
       options: {
         channelId: ['UCHkGnDn8QgN3UlXgnloP2bg'],
         apiKey: process.env.YOUTUBE_API_KEY, // Optional for public requests
-        maxVideos: 50 // Defaults to 50
+        maxVideos: 100 // Defaults to 50
       },
     },
     {
@@ -34,12 +34,7 @@ const config: GatsbyConfig = {
         start_url: "/",
         background_color: "#a471cc",
         theme_color: "#a471cc",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
       },
     },
