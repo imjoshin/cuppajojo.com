08ec417bccaa46a9b0ea3a58087323021760ef61
Josh <jcjohnson77@gmail.com>
2023-01-24 20:12:28 -0500

    Make comets unique shapes

--- src/components/content-background/video-floater/VideoFloater.tsx
+++ src/components/content-background/video-floater/VideoFloater.tsx
@@ -22,10 +22,12 @@ const MOVE_DURATION = {
 export const VideoFloater = ({ image, id, height }: VideoFloaterProps) => {
   const [rotateDuration] = useState(`${Math.floor(Math.random() * (ROTATE_DURATION.max - ROTATE_DURATION.min + 1) + ROTATE_DURATION.min)}s`)
   const [moveDuration] = useState(`${Math.floor(Math.random() * (MOVE_DURATION.max - MOVE_DURATION.min + 1) + MOVE_DURATION.min)}s`)
+  const [cometMask] = useState(Math.floor(Math.random() * (5) + 1))
 
   const imageStyle = {
     backgroundImage: `url(${image})`,
     '--rotate-duration': rotateDuration,
+    '--mask-image': `url("/images/comets/comet${cometMask}.png")`
   }
 
   const floaterStyle = {
