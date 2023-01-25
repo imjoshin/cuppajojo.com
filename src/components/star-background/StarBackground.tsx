7d3ed106a82ed871ce22e24f4c09558a4eab7ba5
Josh <jcjohnson77@gmail.com>
2023-01-24 19:34:04 -0500

    Well I guess I'll write my own star animation because the other one was stupid slow on mobile

--- src/components/star-background/StarBackground.tsx
+++ src/components/star-background/StarBackground.tsx
@@ -3,34 +3,22 @@ import { useRefSize } from "../../hooks/use-ref-size"
 import { StarField } from "./star-field"
 import * as styles from "./StarBackground.module.css"
 
-interface StarBackgroundProps {
-  layers?: number
-}
+const LAYERS = 4
 
-export const StarBackground = ({ layers = 4 }: StarBackgroundProps) => {
+export const StarBackground = () => {
   const ref = React.useRef<HTMLDivElement>();
   const { width, height } = useRefSize(ref)
-  const spread = 50;
-  const duration = 45;
 
-  const starFields = Array(layers)
+  const starFields = Array(LAYERS)
     .fill(0)
     .map((_, index) => (
       <StarField
         key={index}
-        width={width}
-        height={height}
-        spread={spread}
         size={5 - index}
         numStars={(index + 1) * 20}
-        duration={(index + 1) * duration / 4}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
+        duration={(index + 1) * 10}
+        width={width}
+        height={height}
       />
     ))
 
@@ -46,17 +34,7 @@ export const StarBackground = ({ layers = 4 }: StarBackgroundProps) => {
         height: '100%',
       }}
     >
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        {starFields}
-      </div>
+      {starFields}
     </div>
 
   )
--- src/components/star-background/star-field/StarField.tsx
+++ src/components/star-background/star-field/StarField.tsx
@@ -1,111 +1,56 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import * as styles from "./StarField.module.css"
 
-// Started from https://stackblitz.com/edit/react-ts-vp9yex?file=Hero.tsx,StarField.tsx
-
-let id = 0;
-
 type Coords = {
-  x: number
-  y: number
+  x: number,
+  y: number,
 }
 
-interface Props extends React.SVGProps<SVGSVGElement> {
+interface StarFieldProps {
   duration: number
   numStars: number
   size: number
-  spread: number
   width: number
   height: number
 }
 
-export const StarField: React.FC<Props> = ({
-  width,
-  height,
-  spread = 0,
-  size,
-  numStars,
-  duration,
-  ...props
-}) => {
-  id++
+export const StarField = ({ numStars, duration, size, width, height }: StarFieldProps) => {
   const [stars, setStars] = React.useState<Coords[]>([])
-
-  const getRandomInt = (max: number): number => {
-    // return Math.floor(Math.random() * max);
-    return (
-      Math.floor(Math.pow(10, 14) * Math.random() * Math.random()) % (max + 1)
-    )
-  }
-
-  const generateStar = (width: number, height: number): Coords => {
-    return { x: getRandomInt(width), y: getRandomInt(height) }
-  }
 
   React.useEffect(() => {
     const stars = Array(numStars)
       .fill(0)
-      .map(() => generateStar(width, height));
+      .map(() => ({
+        x: Math.floor(Math.random() * width) + 1,
+        y: Math.floor(Math.random() * height) + 1,
+      }));
 
     setStars(stars);
-  }, [width, height]);
+  }, [numStars, width, height]);
+
+  let boxShadow: string[] = []
+
+  for (const star of stars) {
+    boxShadow.push(`${star.x}px ${star.y}px #FFFFFF`)
+    boxShadow.push(`${star.x + width}px ${star.y}px #FFFFFF`)
+  }
+
+  const style = {
+    "--size": `${size}px`,
+    "--fade": `${size}s`,
+    "--duration": `${duration}s`,
+    "--box-shadow": boxShadow.join(`, `),
+    "--page-width": `${width}px`,
+  }
 
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox={`0 0 ${width} ${height}`}
-      width={width}
-      height={height}
+    <div
       className={styles.starField}
-      {...props}
+      // @ts-ignore
+      style={style}
     >
-      <defs>
-        <linearGradient id={`fade-gradient_${id}`} y2={1} x2="0">
-          <stop offset="0" stopColor="white" />
-          <stop offset="0.9" stopColor="white" />
-          <stop offset="1" stopColor="white" stopOpacity="0" />
-        </linearGradient>
 
-        <mask id={`fade-mask_${id}`} maskUnits="userSpaceOnUse">
-          <rect
-            width={width}
-            height={height}
-            fill={`url(#fade-gradient_${id})`}
-          />
-        </mask>
-
-        <filter id={`blur_${id}`}>
-          <feGaussianBlur in="FillPaint" stdDeviation="0.5" />
-        </filter>
-        <g id={`star-field_${id}`} fill="#FFF" filter={`url(#blur_${id})`}>
-          {stars.map(({ x, y }, index) => (
-            <rect
-              key={index}
-              width={size}
-              height={size}
-              x={x}
-              y={y}
-              transform={`rotate(45 ${x} ${y})`}
-            />
-          ))}
-        </g>
-      </defs>
-
-      <g mask={`url(#fade-mask_${id})`} width={width} height={height}>
-        <motion.g
-          animate={{ x: -width }}
-          transition={{
-            ease: 'linear',
-            duration: duration,
-            repeat: Infinity,
-          }}
-        >
-          <use xlinkHref={`#star-field_${id}`} x={0} y={0} />
-          <use xlinkHref={`#star-field_${id}`} x={width} y={0} />
-        </motion.g>
-      </g>
-    </svg>
+    </div>
   )
 }
 
