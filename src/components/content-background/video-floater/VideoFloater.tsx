ec60c59cbc6b4f29b4c1bd7e6e21485383f26080
Josh <jcjohnson77@gmail.com>
2023-01-24 19:49:59 -0500

    Make videos float at different speeds

--- src/components/content-background/video-floater/VideoFloater.tsx
+++ src/components/content-background/video-floater/VideoFloater.tsx
@@ -1,6 +1,6 @@
 
 import { graphql, useStaticQuery } from "gatsby";
-import React from "react"
+import React, { useState } from "react"
 import * as styles from "./VideoFloater.module.css"
 
 interface VideoFloaterProps {
@@ -9,13 +9,28 @@ interface VideoFloaterProps {
   height: number,
 }
 
+const ROTATE_DURATION = {
+  max: 15,
+  min: 5,
+}
+
+const MOVE_DURATION = {
+  max: 30,
+  min: 10,
+}
+
 export const VideoFloater = ({ image, id, height }: VideoFloaterProps) => {
+  const [rotateDuration] = useState(`${Math.floor(Math.random() * (ROTATE_DURATION.max - ROTATE_DURATION.min + 1) + ROTATE_DURATION.min)}s`)
+  const [moveDuration] = useState(`${Math.floor(Math.random() * (MOVE_DURATION.max - MOVE_DURATION.min + 1) + MOVE_DURATION.min)}s`)
+
   const imageStyle = {
-    backgroundImage: `url(${image})`
+    backgroundImage: `url(${image})`,
+    '--rotate-duration': rotateDuration,
   }
 
   const floaterStyle = {
-    top: `${height}%`
+    top: `${height}%`,
+    '--move-duration': moveDuration,
   }
 
   return (
