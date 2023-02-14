402367651eb61879ff2e4e46cad9676e3a17490a
Josh <jcjohnson77@gmail.com>
2023-02-14 00:30:10 -0500

    Get all lines of motd

--- src/api/minecraft.js
+++ src/api/minecraft.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     response = {
       online: true,
       address: MINECRAFT_ADDRESS,
-      motd: r.motd.html[0],
+      motd: r.motd.html.join(`<br />`),
       icon: r.icon,
       version: r.version,
       players,
