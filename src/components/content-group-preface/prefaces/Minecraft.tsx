d2291a5c89c1b3f178e491196d60df3d8b605fd5
Josh <jcjohnson77@gmail.com>
2023-02-14 21:24:27 -0500

    Format mc display

--- src/components/content-group-preface/prefaces/Minecraft.tsx
+++ src/components/content-group-preface/prefaces/Minecraft.tsx
@@ -1,5 +1,24 @@
+import clsx from "clsx"
 import React from "react"
 import { MinecraftStatusProvider, useMinecraftStatus } from "../../../hooks/use-minecraft-status"
+import * as styles from "./Minecraft.module.css"
+
+const Player = ({ avatar, name, delay }: { avatar: string, name: string, delay: number }) => {
+  const style = {
+    animationDelay: `${delay}s`,
+  }
+
+  return (
+    <div className={styles.player} style={style}>
+      <img className={clsx(styles.playerIcon, styles[name.toLowerCase()])} src={avatar} />
+      <div className={styles.playerTag}>
+        <div className={styles.playerTagText}>
+          {name}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const MinecraftDisplay = () => {
   const status = useMinecraftStatus()
@@ -8,19 +27,56 @@ const MinecraftDisplay = () => {
     return null
   }
 
-  const motd = (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: status.motd }} />
+  const address = (
+    <div className={styles.address}>
+      {status.address}
     </div>
   )
+
+  const motd = (
+    <div
+      className={styles.motd}
+      dangerouslySetInnerHTML={{ __html: status.motd }}
+    />
+  )
+
+  const icon = <img src={status.icon} />
+
+  const players = status.players.map((p, i) => (
+    <Player
+      key={p.name}
+      name={p.name}
+      avatar={p.avatar}
+      delay={i / status.players.length / 2}
+    />
+  ))
+
+  const playerSection = status.players.length ? (
+    <div className={styles.playerSection}>
+      <div className={styles.playerInvite}>
+        {status.players.length === 1 ? "I'm" : "We're"} online!
+      </div>
+      <div className={styles.playerContainer}>
+        {players}
+      </div>
+    </div>
+  ) : null
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <img src={status.icon} /><br />
-      {motd}
-      Address: {status.address}<br />
-      Version: {status.version}<br />
-      Player Count: {status.players.length} / {status.maxPlayers}<br />
-      Players: {JSON.stringify(status.players)}<br />
+      <div className={styles.infoContainer}>
+        <div className={styles.icon}>
+          {icon}
+        </div>
+        <div>
+          {address}
+          {motd}
+        </div>
+      </div>
+      <div className={styles.disclaimer}>
+        You must be <a href="/sub" target="_blank">subscribed</a> to CuppaJoJo to join.
+      </div>
+      {playerSection}
     </div>
   )
 }
